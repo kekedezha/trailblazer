23c1b769f958a0d8090c958bee8f1e32c56a9a7e
kekedezha <dezhac6@gmail.com>
2025-08-12 17:11:01 -0400

    updated gitignore, modifying main.py which is entry to fastapi framework

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ app = FastAPI();
 
 @app.get("/")
 def read_root():
-    return {"Hello": "From FastAPI"}
+    return {"message": "Hello from FastAPI"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
